25b51accc0ae47ef8788bc701965aab353843120
jaygajjar111 <jaygajjar.ec@gmail.com>
2026-09-11 17:11:25 +0530

    Add files via upload

---
+++ app/components/RotatingGlobe.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function RotatingGlobe() {
+  return (
+    <div className="rotatingGlobe" aria-label="Rotating digital Earth">
+      <div className="rotatingGlobe__sphere">
+        <div className="rotatingGlobe__map" />
+        <div className="rotatingGlobe__grid" />
+        <div className="rotatingGlobe__shine" />
+        <div className="rotatingGlobe__edge" />
+      </div>
+
+      <div className="rotatingGlobe__orbit rotatingGlobe__orbit--one" />
+      <div className="rotatingGlobe__orbit rotatingGlobe__orbit--two" />
+      <div className="rotatingGlobe__dot rotatingGlobe__dot--one" />
+      <div className="rotatingGlobe__dot rotatingGlobe__dot--two" />
+    </div>
+  );
+}
